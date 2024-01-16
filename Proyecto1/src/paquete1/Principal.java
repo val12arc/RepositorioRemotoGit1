48dfa1d97555172bca6099c7eaaf61cd9b4dadc9
DAW <DAW@DESKTOP-U4FTC0L>
2024-01-16 12:10:09 +0100

    Primera confirmacion

---
+++ Proyecto1/src/paquete1/Principal.java
@@ -0,0 +1,11 @@
+package paquete1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ejemplo de control de versiones con Git");
+		System.out.println("Segundo ejemplo de control de versiones con Git");
+	}
+
+}
