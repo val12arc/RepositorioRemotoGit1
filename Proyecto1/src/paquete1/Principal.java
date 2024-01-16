0cad75c73ea2780148b747c6e9ef9eb3355fd80e
DAW <DAW@DESKTOP-U4FTC0L>
2024-01-16 12:22:04 +0100

    Segundo commit

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -4,8 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Ejemplo de control de versiones con Git");
-		System.out.println("Segundo ejemplo de control de versiones con Git");
+
 	}
 
 }
